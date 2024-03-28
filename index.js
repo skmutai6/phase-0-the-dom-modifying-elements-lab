222d8392a30386023e7068a54012ebde6f16363c
skmutai6 <skmutai6@gmail.com>
2024-03-28 20:42:48 +0300

    complete DOM lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+// const main = document.getElementById("main")
+const element = main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+
+
+
+
+ newHeader.innerHTML = "Shades is the champion"
+
